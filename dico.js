0623677e48d296ba0ca818474f222b1097f4e79c
joycengas <joycengassam056@gmail.com>
2026-02-19 21:12:42 +0100

    ajouter le dico.js dans le logout

---
+++ dico.js
@@ -0,0 +1,26 @@
+
+let dictionnaire = {};
+
+fetch("dictionnaire.json")
+    .then(response => response.json())
+    .then(data => {
+        dictionnaire = data;
+    });
+
+
+
+const input = document.getElementById("mot");
+const bouton = document.getElementById("btn");
+const resultat = document.getElementById("resultat");
+
+bouton.addEventListener("click", function() {
+
+    const motUtilisateur = input.value.toLowerCase();
+
+    if(dictionnaire[motUtilisateur]) {
+        resultat.textContent = dictionnaire[motUtilisateur];
+    } else {
+        resultat.textContent = "Mot non trouvé";
+    }
+
+});
